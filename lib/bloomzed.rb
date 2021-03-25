1f826e1b9910ad557e03700bbede94e3f2bfb1b2
Pavel Tkachenko <tpepost@gmail.com>
2021-03-25 15:35:52 +0600

    Gem initiated

---
+++ lib/bloomzed.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bloomzed/version"
+
+module Bloomzed
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bloomzed_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Bloomzed do
+  it "has a version number" do
+    expect(Bloomzed::VERSION).not_to be nil
+  end
+end
